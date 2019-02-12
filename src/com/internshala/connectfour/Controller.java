b35698d8829edfcebae12bfdba1ee6ef7c1bb817
Rohit Raj <z.rohitraj1996@gmail.com>
2019-02-12 13:50:08 +0530

    We wrote the logic to decide the winner. We define the method gameEnded() checkCombination(),getDiscIfPresent() inside Controller.java.

--- src/com/internshala/connectfour/Controller.java
+++ src/com/internshala/connectfour/Controller.java
@@ -3,6 +3,7 @@ package com.internshala.connectfour;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -17,8 +18,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class Controller implements Initializable{
+public class Controller implements Initializable {
 
     private static final int COLUMNS = 7;
     private static final int ROWS = 6;
@@ -42,47 +45,47 @@ public class Controller implements Initializable{
     @FXML
     public Label playerNameLabel;
 
-    public void createPlayground(){
+    public void createPlayground() {
 
         Shape rectangleWithHoles = createGameStructuralGrid();
         rootGridPane.add(rectangleWithHoles, 0, 1);
 
         List<Rectangle> rectangleList = createClickableColumns();
 
-        for (Rectangle rectangle : rectangleList){
+        for (Rectangle rectangle : rectangleList) {
             rootGridPane.add(rectangle, 0, 1);
         }
     }
 
-    private Shape createGameStructuralGrid(){
+    private Shape createGameStructuralGrid() {
 
-        Shape rectangleWithHoles = new Rectangle((COLUMNS +1 )* CIRCLE_DIAMETER, (ROWS + 1) * CIRCLE_DIAMETER);
+        Shape rectangleWithHoles = new Rectangle((COLUMNS + 1) * CIRCLE_DIAMETER, (ROWS + 1) * CIRCLE_DIAMETER);
 
-        for (int row = 0; row < ROWS; row++){
-            for (int col = 0; col < COLUMNS; col++){
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLUMNS; col++) {
                 Circle circle = new Circle();
                 circle.setRadius(CIRCLE_DIAMETER / 2.0);
                 circle.setCenterX(CIRCLE_DIAMETER / 2.0);
                 circle.setCenterY(CIRCLE_DIAMETER / 2.0);
 
-                circle.setTranslateX(col * (CIRCLE_DIAMETER + 5 ) + CIRCLE_DIAMETER / 4);
+                circle.setTranslateX(col * (CIRCLE_DIAMETER + 5) + CIRCLE_DIAMETER / 4);
                 circle.setTranslateY(row * (CIRCLE_DIAMETER + 5) + CIRCLE_DIAMETER / 4);
                 rectangleWithHoles = Shape.subtract(rectangleWithHoles, circle);
             }
         }
 
         rectangleWithHoles.setFill(Color.WHITE);
-        return  rectangleWithHoles;
+        return rectangleWithHoles;
     }
 
-    private List<Rectangle> createClickableColumns(){
+    private List<Rectangle> createClickableColumns() {
 
         List<Rectangle> rectangleList = new ArrayList<>();
 
-        for (int col = 0; col < COLUMNS; col++){
+        for (int col = 0; col < COLUMNS; col++) {
             Rectangle rectangle = new Rectangle(CIRCLE_DIAMETER, (ROWS + 1) * CIRCLE_DIAMETER);
             rectangle.setFill(Color.TRANSPARENT);
-            rectangle.setTranslateX(col * (CIRCLE_DIAMETER + 5 ) + CIRCLE_DIAMETER / 4);
+            rectangle.setTranslateX(col * (CIRCLE_DIAMETER + 5) + CIRCLE_DIAMETER / 4);
 
             rectangle.setOnMouseEntered(event -> rectangle.setFill(Color.valueOf("#eeeeee26")));
             rectangle.setOnMouseExited(event -> rectangle.setFill(Color.TRANSPARENT));
@@ -97,25 +100,25 @@ public class Controller implements Initializable{
         return rectangleList;
     }
 
-    private void insertDisc(Disc disc, int column){
+    private void insertDisc(Disc disc, int column) {
 
         int row = ROWS - 1;
-        while (row > 0){
+        while (row >= 0) {
 
-            if (insertedDiscsArray[row][column] == null){
+            if (getDiscIfPresent(row, column) == null) {
                 break;
             }
             row--;
         }
 
-        if(row < 0){
+        if (row < 0) {
             return;
         }
 
         insertedDiscsArray[row][column] = disc;       // For Structural changes: For Developers.
         insertedDiscsPane.getChildren().add(disc);
 
-        disc.setTranslateX(column * (CIRCLE_DIAMETER + 5 ) + CIRCLE_DIAMETER / 4);
+        disc.setTranslateX(column * (CIRCLE_DIAMETER + 5) + CIRCLE_DIAMETER / 4);
 
         TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), disc);
         translateTransition.setToY(row * (CIRCLE_DIAMETER + 5) + CIRCLE_DIAMETER / 4);
@@ -123,28 +126,90 @@ public class Controller implements Initializable{
         int currentRow = row;
         translateTransition.setOnFinished(event -> {
 
-            if (gameEnded(currentRow, column)){
-                //TODO.
+            if (gameEnded(currentRow, column)) {
+                gameOver();
+                return;
             }
             isPlayerOneTurn = !isPlayerOneTurn;
-            playerNameLabel.setText(isPlayerOneTurn? PLAYER_ONE : PLAYER_TWO);
+            playerNameLabel.setText(isPlayerOneTurn ? PLAYER_ONE : PLAYER_TWO);
         });
         translateTransition.play();
     }
 
-    private boolean gameEnded(int row, int column){
-        return true;
+    private boolean gameEnded(int row, int column) {
+
+        List<Point2D> verticalPoints = IntStream.rangeClosed(row - 3, row + 3)                      //range of row values = 0,1,2,3,4,5
+                .mapToObj(r -> new Point2D(r, column))     // eg. 0,3  1,3  2,3  3,3  4,3  5,3 --> Point2D x,y
+                .collect(Collectors.toList());
+
+
+        List<Point2D> horizontalPoints = IntStream.rangeClosed(column - 3, column + 3)                      //range of row values = 0,1,2,3,4,5
+                .mapToObj(col -> new Point2D(row, col))     // eg. 0,3  1,3  2,3  3,3  4,3  5,3 --> Point2D x,y
+                .collect(Collectors.toList());
+
+        Point2D startPoint1 = new Point2D(row - 3, column + 3);
+        List<Point2D> diagonal1Points = IntStream.rangeClosed(0, 6)
+                .mapToObj(i -> startPoint1.add(i, -i))
+                .collect(Collectors.toList());
+
+        Point2D startPoint2 = new Point2D(row - 3, column - 3);
+        List<Point2D> diagonal2Points = IntStream.rangeClosed(0, 6)
+                .mapToObj(i -> startPoint2.add(i, i))
+                .collect(Collectors.toList());
+
+        boolean isEnded = checkCombinations(verticalPoints) || checkCombinations(horizontalPoints)
+                || checkCombinations(diagonal1Points) || checkCombinations(diagonal2Points);
+
+        return isEnded;
     }
 
-    private static class Disc extends Circle{
+    private boolean checkCombinations(List<Point2D> points) {
+
+        int chain = 0;
+
+        for (Point2D point : points) {
+
+            int rowIndexForArray = (int) point.getX();
+            int columnIndexForArray = (int) point.getY();
+
+            Disc disc = getDiscIfPresent(rowIndexForArray, columnIndexForArray);
+
+            if (disc != null && disc.isPlayerOneMove == isPlayerOneTurn) {  // If the last inserted Disc belongs to the current player.
+
+                chain++;
+                if (chain == 4) {
+                    return true;
+                }
+            } else {
+                chain = 0;
+            }
+        }
+        return false;
+    }
+
+    private Disc getDiscIfPresent(int row, int column) {     //To prevent ArrayIndexOutOfBoundsException.
+
+        if (row >= ROWS || row < 0 || column >= COLUMNS || column < 0) {  // If row or column is invalid.
+            return null;
+        }
+
+        return insertedDiscsArray[row][column];
+    }
+
+    private void gameOver() {
+        String winner = isPlayerOneTurn ? PLAYER_ONE : PLAYER_TWO;
+        System.out.println("Winner is: " + winner);
+    }
+
+    private static class Disc extends Circle {
 
         private final boolean isPlayerOneMove;
 
-        public Disc(boolean isPlayerOneMove){
+        public Disc(boolean isPlayerOneMove) {
 
             this.isPlayerOneMove = isPlayerOneMove;
             setRadius(CIRCLE_DIAMETER / 2);
-            setFill(isPlayerOneMove? Color.valueOf(DISC_COLOR1) : Color.valueOf(DISC_COLOR2));
+            setFill(isPlayerOneMove ? Color.valueOf(DISC_COLOR1) : Color.valueOf(DISC_COLOR2));
             setCenterX(CIRCLE_DIAMETER / 2);
             setCenterY(CIRCLE_DIAMETER / 2);
         }
